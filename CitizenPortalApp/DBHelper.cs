9c5216cc1750577b36217b214ea66ef715f663d5
Amaan Sheikh <Amaan Sheikh@DESKTOP-V7HQLV7>
2016-06-19 00:49:45 -0700

    Add project files.

---
+++ CitizenPortalApp/DBHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitizenPortalApp
+{
+    class DBHelper
+    {
+        public static string ConnectionString()
+        {
+            return @"Database=citizenPunjab;Data Source=ap-cdbr-azure-southeast-b.cloudapp.net;User Id=b0a7b9b1bc1fa7;Password=fa4e3b97; SslMode=none";
+        }
+    }
+}
