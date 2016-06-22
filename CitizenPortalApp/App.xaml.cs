02ed92f22c7b3a512e176dd292afcefaa2137bfa
Amaan Sheikh <Amaan Sheikh@DESKTOP-V7HQLV7>
2016-06-22 02:47:04 -0700

    created complain design

--- CitizenPortalApp/App.xaml.cs
+++ CitizenPortalApp/App.xaml.cs
@@ -72,7 +72,7 @@ namespace CitizenPortalApp
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(frmLogin), e.Arguments);
+                    rootFrame.Navigate(typeof(frmSignUp), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
